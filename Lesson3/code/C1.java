640667d28d3b91d9109f69251e1700c45357e46b
Aravind Mohan <amohan@Aravinds-MacBook-Pro-916.local>
2020-01-28 09:28:42 -0500

    Graded 01-28-2020 09:27:24

---
+++ Lesson3/code/C1.java
@@ -0,0 +1,10 @@
+package P1;
+public class C1{
+	private int id;
+	public void setID(int id){
+		this.id = id;
+	}
+	public int getID(){
+		return this.id;
+	}
+}
---
+++ Lesson3/code/C2.java
@@ -0,0 +1,8 @@
+package P2;
+public class C2{
+	public static void main(String[] args){
+		C1 obj = new C1();
+		obj.setID(100);
+		System.out.println(obj.getID());
+	}
+}
